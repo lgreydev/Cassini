d843c77449845195e6a502f51f7f9a034a7bac75
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-03-11 20:18:57 +0300

    add image view

---
+++ Cassini/Controllers/ImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Sergey Lukaschuk on 11.03.2022.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+}
