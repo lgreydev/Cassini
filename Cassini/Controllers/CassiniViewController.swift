36aca83ad5f94d8a9c8538c9bf6b7c3c00380e8f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-03-17 10:21:54 +0300

    add extension VC

--- Cassini/Controllers/CassiniViewController.swift
+++ Cassini/Controllers/CassiniViewController.swift
@@ -13,15 +13,23 @@ class CassiniViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             if let url = DemoURLs.NASA[identifier] {
-                var destination = segue.destination
-                if let navcon = destination as? UINavigationController {
-                    destination = navcon.visibleViewController ?? navcon
-                }
-                if let imageVC = destination as? ImageViewController {
+                if let imageVC = segue.destination.contents as? ImageViewController {
                     imageVC.imageURL = url
                     imageVC.title = (sender as? UIButton)?.titleLabel?.text
                 }
             }
+        }
+    }
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+//        } else if let tabcon = self as? UITabBarController {
+//            return tabcon.selectedViewController ?? self
+        } else {
+            return self
         }
     }
 }
